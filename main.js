72563d83d4d8558ecba28e21caa11fb7b249f898
eytan <eytan@chartbeat.com>
2012-07-26 18:24:12 -0400

    progress in mobeat

--- main.js
+++ main.js
@@ -3,35 +3,84 @@ Number.prototype.mod = function(n) {
 }
 
 var PAGE_SELECTOR = '[data-role=page]';
+
+function highlightCurPage(pageIdx) {
+  $(PAGE_SELECTOR).each(function(i, v) {
+    $(v).find('.dot').each(function(index, value) {
+      if (index == pageIdx) {
+        $(value).addClass('active')
+      }
+      else {
+        $(value).removeClass('active')
+      }
+    });
+  });
+}
+
+function onSelectChange(e) {
+  var val = $(this).val();
+  $('.lookbackSelect').each(function(i, v) {
+    $(v).find($("option")).filter(function() {
+        return $(this).val() == val; 
+    }).attr('selected', true);
+  });
+}
+
 function createNav() {
   /* Add Headers */ 
   $(PAGE_SELECTOR).each(function(index, v) {
-    $(v).append('<div data-role="header" data-position="fixed" data-id="header"><h1>Mobeat</h1></div>');
+    $(v).append(
+      "<div class='ui-grid-a' data-role='header' data-position='fixed' data-id='header'> \
+      <div class='logo ui-block-a'></div> \
+      <div class='lookback ui-block-b'></div> \
+      </div>");
   });
 
+  var select = $("<select class='lookbackSelect'> \
+		<option value='1'>1 hour</option> \
+		<option value='2'>2 hours</option> \
+		<option value='12'>12 hours</option> \
+		<option value='24'>24 hours</option> \
+        </select>");
+  $('.lookback').append(select);
+  select.change(onSelectChange);
+  $('.logo').append('<img src="chartbeat-on-black-small.png">')
+  /* Add Footers */ 
   $(PAGE_SELECTOR).each(function(i, v) {
     var footer = $('<div data-role="footer" data-position="fixed" data-id="footer"></div>')
+    var footerContainer = $('<div class="footerContainer"></div>');
     $(v).append(footer);
+    footer.append(footerContainer);
     $(PAGE_SELECTOR).each(function(index, value) {
-      footer.append('<span class="dot"></span>')
+      footerContainer.append('<span class="dot"></span>')
     });
   });
-  /* Add Footers */ 
+}
+
+function getCurPageIdx() {
+  var curPage = ''
+  if (!!$.mobile.activePage) {
+    curPage = $.mobile.activePage.attr('id');
+  } else {
+    var hash = window.location.hash;
+    if (!hash) {
+      return 0
+    }
+    curPage = hash.replace('#', '');
+  }
+  var allPages = $(PAGE_SELECTOR).map(function(i, v) { return $(v).attr('id'); });
+  var curIdx = $.makeArray(allPages).indexOf(curPage);
+  return curIdx;
 }
 
 function changePage(left) {
-  var curPage = $.mobile.activePage.attr('id');
+  var curIdx = getCurPageIdx();
+  var nextIdx = curIdx + (left ? 1 : -1);
   var allPages = $(PAGE_SELECTOR).map(function(i, v) { return $(v).attr('id'); });
-  var curIdx = $.makeArray(allPages).indexOf(curPage);
-  var nextIdx = curIdx + (left ? -1 : 1);
   nextIdx = (nextIdx > 0) ? nextIdx : -1 * nextIdx;
   nextIdx = nextIdx % (allPages.length);
   var nextPage = allPages[nextIdx];
-  /*
-  var footer = $('#footer');
-  footer.detach();
-  $('#' + nextPage).append(footer);
-  */
+  highlightCurPage(nextIdx);
   $.mobile.changePage('#' + nextPage, "slide");
 }
 
@@ -40,7 +89,34 @@ function attachNavEvents() {
   $(document).swipeleft(function() { changePage(true) });
 }
 
-createNav();
+/*
+function instanceGetter(fields) {
+  return function(domain, apikey, content) {
+    return new Toppages
+  }
+}
+*/
+pages = {
+  'toppages': Toppages,
+  'engagement': EngagedPages,
+}
+
+function initPages() {
+  $(PAGE_SELECTOR).each(function(i, v) { 
+    var pageId = $(v).attr('id');
+    var pageClass = pages[pageId];
+    if (pageClass) {
+      console.log($(v));
+      var content = $($(v).find('[data-role=content]')[0]);
+      console.log(content);
+      var page = new pageClass('nytimes.com', '', content); 
+      page.refresh();
+    }
+  });
+}
 $(function() {
+  createNav();
+  highlightCurPage(getCurPageIdx());
   attachNavEvents();
+  initPages();
 });
---
+++ toppages.js
@@ -0,0 +1,57 @@
+function Toppages(host, apikey, domObj){  
+  this._host = host;
+  this._apikey = apikey;
+  this._dom = domObj;
+}
+
+Toppages.prototype.getFields = function() {
+  return 'read,write,idle';
+}
+
+Toppages.prototype.onData = function(data) {
+  this._dom.empty();
+  var that = this;
+  var listview = $('<ul data-role="listview" data-theme="b"></ul>');
+  var data = data['data']['top'];
+  $.each(data, function(i, v) {
+    listview.append("<li><a href='page.html' data-transition='flip'>%visitors - %title</a></li>".
+        replace(/%title/, v['title']).
+        replace(/%visitors/, v['val']));
+  });
+  that._dom.append(listview);
+  if (this.init()) {
+    listview.listview();
+  }
+}
+
+Toppages.prototype.init = function() {
+  return true;
+}
+
+Toppages.prototype.refresh = function() {
+  var url = 'http://192.168.116.43:2512/top/';
+  data = {
+    'apikey': this._apikey,
+    'host': this._host,
+    'fields': this.getFields(),
+  };
+  $.ajax({
+    url: url,
+    data: data,
+    dataType: 'jsonp',
+    jsonp: 'jsonp',
+    success: $.proxy(this.onData, this),
+  });
+}
+
+EngagedPages.prototype = new Toppages();
+function EngagedPages(host, apikey, domObj){
+    Toppages.call(this, host, apikey, domObj);
+}
+EngagedPages.prototype.getFields = function() {
+  return 'read,write';
+}
+
+EngagedPages.prototype.init = function() {
+  return false;
+}
